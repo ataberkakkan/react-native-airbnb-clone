5eae6c73c52ad97c40961baef4a43cfd9be25e26
ataberkakkan <akkanataberk@gmail.com>
2024-03-08 12:15:56 +0300

    Clerk Auth and Log In Screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { View, Text, Button } from "react-native";
 
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Button title="Log Out" onPress={() => signOut()} />
+      {!isSignedIn && <Link href={"/(modals)/login"}>Log In</Link>}
     </View>
   );
 };
